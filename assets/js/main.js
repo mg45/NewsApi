09b657a9c7be082004234a165453289910da52d5
mg45 <m.golabski@gmx.de>
2021-03-10 10:27:35 +0100

    check response

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,10 +1,17 @@
+const newsContainer = document.getElementById("news-container");
+
 let fetchUriTechCrunch = "http://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=";
 let apiKey = "18d8c403272e46099d86fa39edcb3e75";
 
 
 fetch(fetchUriTechCrunch + apiKey)
-  .then((response) => response.json())
-  .then((responseData) => {
-    console.log(responseData);
+  .then(response => response.json())
+  .then((data) => {
+    data.articles.forEach(element => {
+      newsContainer.innerHTML +=
+      `
+      <div>${element.title}</div>
+      `
+    });   
   });
 
